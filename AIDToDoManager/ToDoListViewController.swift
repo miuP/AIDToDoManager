7fafda6849d85812b0cca569d53570d73dacc12b
KazuyaMIURA <miup.0627@gmail.com>
2015-07-24 18:19:35 +0900

    add ToDoListViewController

---
+++ AIDToDoManager/ToDoListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ToDoListViewController.swift
+//  AIDToDoManager
+//
+//  Created by IllyasvielVonEinzbern on 7/24/15.
+//  Copyright (c) 2015 KazuyaMIURA. All rights reserved.
+//
+
+import UIKit
+
+class ToDoListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
